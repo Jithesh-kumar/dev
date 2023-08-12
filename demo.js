f1d9d70695896c5b1276ae05baa128070974c7d7
Jithesh kumar M S <jitheshkumarjithu8@gmail.com>
2023-08-12 10:05:37 +0530

    New

--- demo.js
+++ demo.js
@@ -1,1 +1,17 @@
-let a = 10;
+// const people = [
+//     { id: 1, name: 'Alice', age: 25 },
+//     { id: 2, name: 'Bob', age: 30 },
+//     { id: 3, name: 'Charlie', age: 22 }
+// ];
+
+// const peopleList = document.getElementById('people-list');
+
+// // Use the map method to generate HTML for each person
+// const peopleHtml = people.map(person => {
+//     return `<li>${person.name}, Age: ${person.age}</li>`;
+// });
+
+// // Join the generated HTML and add it to the list
+// peopleList.innerHTML = peopleHtml.join('');
+export const name = "Jesse";
+export const age = 40;
